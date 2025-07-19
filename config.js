a9d7cfba8ef20279ec8f56bddcafec9412ffa01a
DilaX10 <dilshanx328@gmail.com>
2025-07-19 01:26:12 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "5chX3LrQ#86o_FOM2RdIUYDMHP0YlnrH6uG0hQVccAobAlNARVJc",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DilaX10/Dilshan-MD/blob/main/images/20250719_115945.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DILSHAN-MD Is Alive Now😍*",
 BOT_OWNER: '94743376317',  // Replace with the owner's phone number
