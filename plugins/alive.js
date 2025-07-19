96beb78501329b4677971a39e92fcd5ec2d3ed8f
DilaX10 <dilshanx328@gmail.com>
2025-07-19 00:24:15 -0700

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,7 +3,27 @@ const config = require('../config');
 
 cmd({
     pattern: "alive",
-    desc: "Check bot online or no.",
+    desc: "╭───〔 ✅ I'M ALIVE NOW 〕───⬣
+│
+│ 🔹 Bot Name: 🔮 DILSHAN-MD 🔮
+│ 🔹 Status: Online & Active ✅ 
+│ 🔹 Ping: 0 ms
+│ 🔹 Uptime: 00:01:06
+│ 🔹 Platform: linux
+│ 🔹 CPU: AMD EPYC 7B13
+│ 🔹 Owner: DILSHAN CHANUSHKA
+│ 🔹 Version: 1.0.0
+│
+╰───────────────⬣
+
+📡 Stay Connected with Me!
+🎥 Watch & Learn Tech with Djz Dilshan X Jay
+🔔 Subscribe Now: 
+👉 
+https://youtube.com/@djzdilshanxjay?si=9XdKXTq12AApPFMP
+─────────────────────────
+🚀 Powered By  DILSHAN CHANUSHKA 🔥
+",
     category: "main",
     filename: __filename
 },
